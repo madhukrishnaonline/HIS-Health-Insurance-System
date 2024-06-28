19e45e999e5c47f1c976c9ff1e161fa0ae266fd7
Madhu Krishna <144832720+madhukrishnaonline@users.noreply.github.com>
2024-06-28 14:52:38 +0530

    Delete SpringBoot-CRUD_Operations2 directory

--- SpringBoot-CRUD_Operations2/src/main/java/com/mk/repository/EmployeeRepopository.java
+++
@@ -1,9 +0,0 @@
-package com.mk.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mk.entity.EmployeeEntity;
-
-public interface EmployeeRepopository extends JpaRepository<EmployeeEntity, Integer>{
-
-}
